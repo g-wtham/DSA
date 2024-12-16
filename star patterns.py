1b29d4f86eaad9c0ddabc460b5ebebfc318a0056
Gowtham <curiousgowtham@gmail.com>
2024-12-16 23:10:16 +0530

    diamond_triangle

--- star patterns.py
+++ star patterns.py
@@ -12,12 +12,48 @@ in :
     
 '''
 
-n = 5
-for i in range(n):
-    for x in range(i):
-        print(" ", end="")
-    for y in range(2 * n - (2 * i +1)):
-        print("*", end="")
-    for z in range(i):
-        print(" ", end="")
-    print()
+def inverted_triangle():
+    n = 5
+    for i in range(n):
+        for x in range(i):
+            print(" ", end="")
+        for y in range(2 * n - (2 * i +1)):
+            print("*", end="")
+        for z in range(i):
+            print(" ", end="")
+        print()
+
+inverted_triangle()
+    
+'''
+Diamond shaped triangle :
+
+ol : range(n) -> rows -> n
+il : 
+    1st loop: space
+    2nd loop: stars
+    3rd loop: space
+    
+repeat for inverted triangle
+
+[4, 1, 4] 0 -> as 1 = +1
+[3, 3, 3] 1 -> as 3 = +2
+[2, 5, 2] 2 -> as 5 + +3
+[1, 7, 1] 3
+[0, 9, 0] 4 
+'''
+def diamond_triangle():
+    n = 5
+    for i in range(n):
+        for x in range(n-1-i):
+            print(" ", end="")
+        for y in range(2 * i + 1):
+            print("*", end="")
+        for z in range(n-1-i):
+            print(" ", end="")
+        print()
+    inverted_triangle()
+
+diamond_triangle()
+        
+    
