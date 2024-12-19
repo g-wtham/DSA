96cc48e6db64b1bb7ddfda8ed8dde3744c359843
Gowtham <curiousgowtham@gmail.com>
2024-12-19 23:53:47 +0530

    prime numbers

--- basic_maths_problems.py
+++ basic_maths_problems.py
@@ -12,3 +12,23 @@ def print_all_divisors(n):
         print(j)
     
 print_all_divisors(10)
+
+def prime_numbers(n): 
+    count_prime = 0  
+    for i in range(1, int(math.sqrt(n) + 1)):
+        if (n%i == 0):
+            count_prime += 1
+            if ((n/i) != i):
+                count_prime += 1
+    
+    '''
+    The condition if ((n / i) != i) ensures that for divisors i, the "mirror" divisor n / i is only counted if it is distinct from i. 
+    This is especially useful for avoiding double-counting in the case of perfect squares.
+    '''        
+                
+    if count_prime == 2:
+        print("Prime")
+    else:
+        print("Not prime")
+
+prime_numbers(789)
