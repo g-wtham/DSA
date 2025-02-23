7c02ae9fb5f872858b743c575403daefa60943da
Gowtham <curiousgowtham@gmail.com>
2025-02-23 13:45:46 +0530

    stack

---
+++ stack/valid parantheses.py
@@ -0,0 +1,22 @@
+'''
+Given a string s, composed of different combinations of '(' , ')', '{', '}', '[', ']', verify the validity of the arrangement.
+An input string is valid if:
+
+         1. Open brackets must be closed by the same type of brackets.
+         2. Open brackets must be closed in the correct order.
+'''
+def isBalanced(s):
+    me = {')' : '(', '}':'{', ']' : '['}
+    stack = []
+    for char in s:
+        if char in '({[':
+            stack.append(char)
+        elif char in ')}]':
+            if not stack:
+                return False
+            top = stack.pop()
+            if top != me[char]:
+                return False
+    return not stack
+
+print(isBalanced('[{}]'))
