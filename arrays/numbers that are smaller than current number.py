eb050961d94595a409d52a03a51b53695cff3485
Gowtham <curiousgowtham@gmail.com>
2025-03-04 22:07:00 +0530

    numbers smaller than current number

---
+++ arrays/numbers that are smaller than current number.py
@@ -0,0 +1,9 @@
+arr = [1,7,2, 5, 6, 9]
+
+for i in range(len(arr)):
+    count = 0
+    for j in range(len(arr)):
+        if arr[j] < arr[i]:
+            count += 1
+            
+    print("Smaller than", arr[i], " = ", count)
