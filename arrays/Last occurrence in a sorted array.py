5e1356224f2e10785b86439196f5136c483e6c89
Gowtham <curiousgowtham@gmail.com>
2025-03-09 20:24:02 +0530

    first and last occurence

--- arrays/Last occurrence in a sorted array.py
+++ arrays/Last occurrence in a sorted array.py
@@ -3,20 +3,45 @@
 target=13
 arr = [3,4,13,13,13,20,40]
 
-left = 0
-right = len(arr)
-result = -1
+def last_occur(target, arr):
+    left = 0
+    right = len(arr)
+    result1 = -1
 
-while left <= right:
-    mid = (left+right) // 2
-    if arr[mid] == target:
-        result = mid
-        left = mid + 1
+    while left <= right:
+        mid = (left+right) // 2
+        if arr[mid] == target:
+            result1 = mid
+            left = mid + 1
         
-    elif arr[mid] < target:
-        left = mid + 1
+        elif arr[mid] < target:
+            left = mid + 1
         
-    else:
-        right = mid - 1
+        else:
+            right = mid - 1
+    return result1
+
+
+def first_occur(target, arr):
+    left = 0
+    right = len(arr)
+    result2 = -1
+
+    while left <= right:
+        mid = (left+right) // 2
+        if arr[mid] == target:
+            result2 = mid
+            right = mid - 1
         
-print(result)
+        elif arr[mid] < target:
+            left = mid + 1
+        
+        else:
+            right = mid - 1
+    return result2
+
+last_occur = last_occur(target, arr)
+first_occur = first_occur(target, arr)
+
+        
+print("Target", target, "first occurs at index", first_occur, "and last occurs at index", last_occur)
