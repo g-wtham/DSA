8c12f15d4656013740088a44aa0cce6a106bb8c2
Gowtham <curiousgowtham@gmail.com>
2025-03-09 16:19:55 +0530

    using set and xor methods

--- arrays/missing_number.py
+++ arrays/missing_number.py
@@ -11,3 +11,27 @@ for i in range(1, total_n+1):
             
     if not found:
         print(i)
+        
+# Using XOR approach - but only finds one missing number, not multiple missing numbers!
+nums = [1, 2, 3, 5]
+
+def xor_approach(nums):
+    xor = 0
+    for i in range(1, max(nums)+1):
+        xor ^= i
+        
+    for item in nums:
+        xor ^= item
+    
+    return xor
+            
+print(xor_approach(nums))
+
+arr = [1, 9, 7, 2, 5, 6]
+
+items = set(arr)
+missing_num = []
+for i in range(1, max(items)+1):
+    if i not in items:
+        missing_num.append(i)
+print(missing_num)
