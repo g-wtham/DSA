fd8d18bc75c5f3a7a19b207425d6f7e72124e1cb
Gowtham <curiousgowtham@gmail.com>
2025-03-11 21:42:33 +0530

    hashmap - O(n)

--- arrays/Two Sum.py
+++ arrays/Two Sum.py
@@ -7,4 +7,18 @@ for i in range(len(nums)):
             list1.append(i)
             list1.append(j)
     
-print(list1)        
+print(list1)  
+
+# Optimal approach using HashMap (dict.)
+
+def two_sum(nums, target):
+    nums_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        
+        if complement in nums_map:
+            return [nums_map[complement], i]     
+        
+        nums_map[num] = i 
+    
+print(two_sum(nums, target))
