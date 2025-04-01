88459029b3da104a5a107c845a92cfbc67944f14
Gowtham <curiousgowtham@gmail.com>
2025-04-01 11:24:47 +0530

    2nd way

--- strings/reverse words in a string.py
+++ strings/reverse words in a string.py
@@ -6,3 +6,15 @@ rev_word = splitted_string[::-1]
 
 print(" ".join(rev_word))
     
+
+# 2nd time practice:
+
+str = 'word is a game'
+for char in str:
+    splitted = str.split(' ')
+    rev = splitted[::-1]
+print(rev)
+
+str = 'word is a game'
+rev = str[::-1]
+print(rev)
