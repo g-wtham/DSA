708ee353649784dc760879a6e70f8e1a24b1a979
Gowtham <curiousgowtham@gmail.com>
2025-04-16 22:35:55 +0530

    stack operations

---
+++ stack/Infix, Postfix, Prefix.py
@@ -0,0 +1,14 @@
+# A + B - C * D
+
+'''
+i   stack   answer
+0           A
+1    +      A
+2    +      AB
+3    -      AB+
+4    -      AB+C
+5    -*     AB+C
+6    -*     AB+CD
+
+Answer : AB+CD*-
+'''
