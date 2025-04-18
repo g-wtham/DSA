e709d3dd5c91c73cdd49a8999f6359ada0188d92
Gowtham <curiousgowtham@gmail.com>
2025-04-18 19:22:25 +0530

    transpose

--- arrays/multidimensional arrays.py
+++ arrays/multidimensional arrays.py
@@ -21,3 +21,25 @@ B = [[9, 8, 7],
      [3, 2, 1]]   
     
 print(addMatrix(A, B))
+
+
+def transpose(a):
+    rows = len(a)
+    col = len(a[0])
+    
+    result = []
+    for _ in range(rows):
+        result.append([0] * col)
+        
+    for i in range(rows):
+        for j in range(col):
+            result[i][j] = a[j][i]
+        
+    for row in result:
+        print(row)
+    
+A = [[1, 2, 3],  
+     [4, 5, 6],  
+     [7, 8, 9]]    
+    
+transpose(A)
