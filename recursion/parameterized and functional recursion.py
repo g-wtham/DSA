ebfc0f19788c4fee4dc92cea248ad63895041220
Gowtham <curiousgowtham@gmail.com>
2025-04-27 00:08:50 +0530

    recursion pt.2

---
+++ recursion/parameterized and functional recursion.py
@@ -0,0 +1,41 @@
+def f(n, sum):
+    if n<1:
+        print(sum)
+        return 
+    
+    f(n-1, sum+n)
+
+n = 5
+f(5, 0)
+
+def f(i, n):
+    if n==0:
+        print(i)
+        return
+    
+    f(i+n, n-1)
+    
+n = 5
+f(0, 5)
+
+def f(i, n):
+    if i > n:
+        return 1
+    return i + f(i+1, n)
+
+n = 5
+f(0, 5)
+
+# If you are starting from 0 means, you are adding 0 at end when the call stack unwinds, which adds nothing and so no change
+# If you start from 5 and put 0 (returned) for condition as i > n, it will work as it will return 0.
+# If you start from 5 and put 1 (returned) for condition as i > n, it will like add one extra value.
+# Eg: 6 > 5 => return 0 ----- 5 + 0 (crct) ; 6 > 5 => return 1 ----- 5 + 1 (wrong) [5+1, 4+6, 3+10, 2+13, 1+15 ==> 16]
+
+
+def factorial(n):
+    if n==1:
+        return 1
+    
+    return n * factorial(n-1) 
+    
+print(factorial(10))
