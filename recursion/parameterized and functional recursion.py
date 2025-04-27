11a34bfc39ae45a16587d4b743a9d16c7be575ce
Gowtham <curiousgowtham@gmail.com>
2025-04-27 23:18:54 +0530

    reverse an array using recursion

--- recursion/parameterized and functional recursion.py
+++ recursion/parameterized and functional recursion.py
@@ -39,3 +39,32 @@ def factorial(n):
     return n * factorial(n-1) 
     
 print(factorial(10))
+
+arr = [1,2,3,4,5]
+i=0
+j=len(arr)-1
+
+while i<j:
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+    i+=1
+    j-=1
+    
+print(arr)
+
+def swap(i, j):
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+    
+arr = [1,2,3,4,5]
+
+def f(i, j):
+    if i>j:
+        return
+    swap(i, j)
+    f(i+1, j-1)
+
+f(0, 4)    
+print(arr)
