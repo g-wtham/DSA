957677496da8c6aaba7afa0687141b317278716a
Gowtham <curiousgowtham@gmail.com>
2025-05-16 20:03:18 +0530

    more patterns

--- star patterns revision + update.py
+++ star patterns revision + update.py
@@ -256,3 +256,58 @@ for i in range(n):
     print()
     
 # %%
+
+for i in range(n):
+    for j in range(n-i, 0, -1):
+        print("*", end=" ")
+    for j in range(n, 0, -1):
+        print("*", end=" ")
+    print()
+# %%
+for i in range(n, 0, -1):
+    print(" " * (n-i) + "*" * i)
+# %%
+n = 5
+
+for i in range(n):
+    for j in range(n-i, 0, -1):
+        print("*", end="")
+    for _ in range(2*i):
+        print(" ", end="")
+    for k in range(n-i, 0, -1):
+        print("*", end="")
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print("*", end="")
+    for _ in range(2*(n-i-1)):
+        print(" ", end="")
+    for k in range(i+1):
+        print("*", end="")
+    print()
+
+    
+# %%
+
+# ButterFly Pattern
+
+n = 5
+
+for i in range(n):
+    for j in range(i+1):
+        print("*", end="")
+    for j in range(2*(n-i-1)):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*", end="")
+    print()
+for i in range(n-1, 0, -1):
+    for j in range(i):
+        print("*", end="")
+    for j in range(2*(n-i)):
+        print(" ",end="")
+    for j in range(i):
+        print("*", end="") 
+    print()
+
+# %%
