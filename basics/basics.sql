904835b9ba5f606b2cfb1dbcbac3d2b4f32b4259
Gowtham <curiousgowtham@gmail.com>
2025-07-14 22:32:09 +0530

    sql

--- basics/basics.sql
+++ basics/basics.sql
@@ -2,3 +2,12 @@ SELECT COUNT(DISTINCT Price)
 FROM Products;
 
 SELECT COUNT(Products) AS [Number Of Products], CategoryId From Products GROUP BY CategoryId;
+
+'''
+SQL Joins
+'''
+SELECT Products.ProductID, Products.ProductName, Categories.CategoryName from Products INNER JOIN Categories ON Products.CategoryID = Categories.CategoryID;
+
+'''
+If columns are present in both tables, then mention table names along with column name. If not, if columns are different in both table, just mentioning column name is enough.
+
