12e3a4d7350dfbcc2e66cf80662e597973041116
Gowtham <curiousgowtham@gmail.com>
2025-07-23 22:48:40 +0530

    sql

--- basics/basics.sql
+++ basics/basics.sql
@@ -11,3 +11,20 @@ SELECT Products.ProductID, Products.ProductName, Categories.CategoryName from Pr
 '''
 If columns are present in both tables, then mention table names along with column name. If not, if columns are different in both table, just mentioning column name is enough.
 
+'''
+
+-- Aggregate Functions 
+
+select s.name, count(m.subject) as subjects_count from students s JOIN marks m ON m.student_id = s.student_id group by s.name;
+
+select s.name, sum(m.score) as sum_value from students s JOIN marks m ON m.student_id = s.student_id group by s.name HAVING sum(m.score) > 150;
+
+select m.subject, avg(m.score) as AVG_SUM from students s JOIN marks m ON m.student_id = s.student_id GROUP BY m.subject ;
+
+select s.name, sum(m.score) from students s JOIN marks m ON m.student_id = s.student_id group by s.name;
+
+delete from marks where student_id = 4;
+
+ALTER TABLE marks rename column score to total_score;
+
+ALTER marks add column 'score';
