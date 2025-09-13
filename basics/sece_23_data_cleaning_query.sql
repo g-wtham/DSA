1318dcdd70e7e4f9a28e91f60b0bdcdcf5740582
Gowtham <106096598+g-wtham@users.noreply.github.com>
2025-09-13 10:41:34 +0530

    salary_range query added

--- basics/sece_23_data_cleaning_query.sql
+++ basics/sece_23_data_cleaning_query.sql
@@ -34,8 +34,32 @@ update sece_23 set company_name =  trim(substring_index(company_name, '-', 1));
 
 SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'sece_23' AND COLUMN_NAME = 'salary';
 
+SET SQL_SAFE_UPDATES = 0;
+
 UPDATE sece_23 SET salary = CAST(REPLACE(salary, ',', '') AS UNSIGNED);
 
 select * from sece_23 order by salary desc;
+
+ALTER TABLE sece_23 MODIFY salary INT UNSIGNED;
+
+SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'sece_23' AND COLUMN_NAME = 'salary';
+
+alter table sece_23 add column salary_range text;
+
+update sece_23 set salary_range = (CASE 
+	WHEN salary >= 700000 THEN 'ABOVE_7_LPA'
+    WHEN salary >= 500000 THEN '5_TO_7_LPA'
+    WHEN salary >= 400000 THEN '4_TO_5_LPA'
+    WHEN salary >= 300000 THEN '3_TO_4_LPA'
+    ELSE 'BELOW_3_LPA'
+    END
+);
+
+select count(student_name) from sece_23;
+
+SELECT salary_range, count(student_name) as number_of_students from sece_23
+GROUP BY salary_range ORDER BY number_of_students desc;
+
+
 
 
